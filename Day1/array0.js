b5d4801482c866814fb654b6f1c5978742ee5bd1
shadab057 <mohdshadab4629@gmail.com>
2025-04-29 19:07:17 +0530

    Explained why arrays are better than objects for ordered data handling

---
+++ Day1/array0.js
@@ -0,0 +1,39 @@
+// let colors = ["red", "green", "blue"];
+// console.log(colors[1])
+
+// let suppose we have to store five students marks
+
+
+//  CASE:1
+let mark1_s1 = 97;
+let mark1_s2 = 45;
+let mark1_s3= 86;
+let mark1_s4 = 77;
+let mark1_s5 = 72;
+
+// this approach is not a ideal practical and real-world for storing a students data.
+// • Repetition and Complexity
+// • Code maintaining tough
+// • Can't use Loop
+// • Readability and scalability 
+
+
+// CASE:2 
+// we can create a objects
+let marks = {
+    s1:97,
+    s2:68,
+    s3:77,
+    s4:89,
+    s5:90
+}
+
+// This approach might be good in readability but especially when data in large scale 
+
+// • difficult to loop 
+// • if subject names change and dynamic have some issue.
+// • no index-based access in objects
+// • managing multiple students becomes complex.
+
+
+
