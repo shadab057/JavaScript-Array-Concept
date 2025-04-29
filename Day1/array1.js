19cb8e8cd0cc074916aaf8cf0d312a5666c50b89
shadab057 <mohdshadab4629@gmail.com>
2025-04-29 19:07:53 +0530

    Introduced array concept with syntax and basic usage

---
+++ Day1/array1.js
@@ -0,0 +1,9 @@
+// let fruits = ["apple", "banana", "mango"]
+// console.log(fruits);   //  ["apple" "banana" "mango"]
+
+
+// let arrayName = [value1, value2, value3]
+// console.log(arrayName);
+
+let marks = [97, 83, 75, 64, 36]
+console.log(marks)
